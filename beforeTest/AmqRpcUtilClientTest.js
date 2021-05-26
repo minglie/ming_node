edd0ca066524b2b6100168c21f9f23bfca2ed343
jihefu <1744264136@qq.com>
2021-05-26 20:43:38 +0800

    AAAAA

--- beforeTest/AmqRpcUtilClientTest.js
+++ beforeTest/AmqRpcUtilClientTest.js
@@ -5,7 +5,7 @@ rabitMqConfig= {
     rabbitMQPassword:"guest",
     rabbitMQHost:"localhost",
     rabbitMQPort:5672,
-    rpcQueueClient:"rpc_queue_client",
+    rpcQueueClient:"rpc_queue_client111",
     rpcQueueServer:"rpc_queue_server",
     mode:"client"  //如果是server 会开启监听
 }
